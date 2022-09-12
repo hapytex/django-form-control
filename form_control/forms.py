0f79e088ada257822e7d58074e261ea89bb23b26
Willem Van Onsem <vanonsem.willem@gmail.com>
2022-09-12 12:12:27 +0200

    work with CI/CD of GitHub

--- form_control/forms.py
+++ form_control/forms.py
@@ -3,17 +3,27 @@ from form_control.constants import FORM_CONTROL
 
 class FormControlMixin:
     form_control_fields = None
+    form_control = FORM_CONTROL
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            # a dict retains the order
-            cls = dict.fromkeys(field.widget.attrs.get('class', '').strip().split())
-            if FROM_CONTROL not in cls:
-                cls[FORM_CONTROL] = None
-                field.widget.attrs['class'] = ' '.join(cls)
+        if self.form_control_fields is None:
+            fields = self.fields.values()
+        else:
+            fields = map(self.fields.get, self.form_control_fields)
+        form_control_attr = self.form_control
+        for field in fields:
+            if field is not None:
+                attrs = field.widget.attrs
+                # a dict retains the order
+                cls = dict.fromkeys(attrs.get('class', '').strip().split())
+                if form_control_attr not in cls:
+                    cls[form_control_attr] = None
+                    attrs['class'] = ' '.join(cls)
+
 
 class FormControlForm(FormControlMixin, forms.Form):
     pass
+
 
 class FormControlModelForm(FormControlMixin, forms.ModelForm):
     pass
